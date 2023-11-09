d5f52b8642556fbe088cca258197fd7aba5c22bf
lilmirpnb <144713314+lilmirpnb@users.noreply.github.com>
2023-11-09 17:02:00 +0000

    async work oct 30th kahmere d

---
+++ async work oct 30th.py
@@ -0,0 +1,29 @@
+ List and explain three 3 reasons why programmers write documentation. 
+
+explains all the features of a project informs us how we can work with them 
+helps to understand the projects's functionalituy and allows us to reduce onboarding time and costs
+
+
+2. In your own words, describe what a README.md document is.
+
+ intended to provide orientation for engineers browsing your code, especially first-time users. 
+
+
+
+3. Based on what your read, what are four (4) things that should be in a README document. 
+
+installation
+
+LICENSE file. 
+
+Contributing
+
+Description
+
+4. Name and describe three (3) reasons why programmers write commmit messages when using GitHub.
+
+Being able to see what you were thinking months or years ago when you made a change.
+Being able to see what other engineers were thinking when they made their changes.
+Having a clear chronological log of work you can look back through.
+General spreading of knowledge and awareness in a team.
+
